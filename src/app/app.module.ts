1eba78e14d4572ab27c37e48b55ef7c7fd6e33f2
Robert Raboud <robert.raboud@outlook.com>
2017-12-31 14:11:37 -0500

    STEP 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoggerModule } from './logger/logger.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LoggerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/logger/logger.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { LoggerRoutingModule } from './logger-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    LoggerRoutingModule
+  ],
+  declarations: []
+})
+export class LoggerModule { }
