edf59c69d16376f3864ce51de9f58a962e9ee5b2
Robert Raboud <robert.raboud@outlook.com>
2017-12-31 14:13:10 -0500

    STEP 5

--- src/app/logger/logger.module.ts
+++ src/app/logger/logger.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoggerRoutingModule } from './logger-routing.module';
+import { LoggerService } from './logger.service';
 
 @NgModule({
   imports: [
     CommonModule,
     LoggerRoutingModule
   ],
-  declarations: []
+  declarations: [],
+  providers: [LoggerService]
 })
 export class LoggerModule { }
